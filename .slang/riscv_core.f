bf886b8a00faa2db95a6daa3381c9e4e31ad1703
L1Ban <libohangm@gmail.com>
2026-06-18 17:05:20 +0800

    chore: configure rtl editor tooling

---
+++ .slang/riscv_core.f
@@ -0,0 +1,13 @@
+-I rtl/include
+--top riscv_core
+rtl/include/riscv_core_pkg.sv
+rtl/core/riscv_core.sv
+rtl/core/pipe/if_stage.sv
+rtl/core/pipe/id_stage.sv
+rtl/core/pipe/ex_stage.sv
+rtl/core/pipe/mem_stage.sv
+rtl/core/pipe/wb_stage.sv
+rtl/core/units/alu.sv
+rtl/core/units/decoder.sv
+rtl/core/units/imm_gen.sv
+rtl/core/units/regfile.sv
